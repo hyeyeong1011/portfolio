25d1eb550df0371773e3705328ebe9cd3353c5f0
hyeyeong1011 <52442799+hyeyeong1011@users.noreply.github.com>
2020-02-20 18:03:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ $(function(){
 		if (!obj){	// 예외처리, 현재는 기능적으로 필요한 부분은 아님, 관례적 사용
 			$('html, body').animate({scrollTop:0},speed);
 		}else{
-			var posTop = $(obj).offset().top -80;	// posTop = 매개변수로 들어온 컨텐츠 obj 의 offset().top - 네비게이션 높이
+			var posTop = $(obj).offset().top -60;	// posTop = 매개변수로 들어온 컨텐츠 obj 의 offset().top - 네비게이션 높이
 			$('html, body').animate({scrollTop:posTop}, speed )	// body의 스크롤이동 : posTop
 		};
 	};
@@ -95,27 +95,46 @@ $(function(){
 
 
 
+
     // 프로그레스 바
+    // $(window).scroll(function(){
+        
+    //     var scrollTop=$(this).scrollTop();
+    //     var skills=$('#skills').offset().top-500;
+        
+    //     if(scrollTop>=skills){
+
+    //         $('.progress-bar').each(function(){
+
+    //             $(this).find('.bar').animate({
+    //                 width:$(this).data('percent')
+    //             },2000);
+
+    //         });
+
+    //     }
+    // });
+
+
+
+
+    // skills 영역 애니메이션
     $(window).scroll(function(){
-        
         var scrollTop=$(this).scrollTop();
-        var skills=$('#skills').offset().top-500;
-        
+        var skills=$('#skills').offset().top-300;
+
         if(scrollTop>=skills){
 
-            $('.progress-bar').each(function(){
-
-                $(this).find('.bar').animate({
-                    width:$(this).data('percent')
-                },2000);
-
-            });
+            $(".skill-item:nth-child(1)").addClass('flip-bottom-1');
+            $(".skill-item:nth-child(2)").addClass('flip-bottom-2');
+            $(".skill-item:nth-child(3)").addClass('flip-bottom-3');
+            $(".skill-more").addClass('flip-bottom-4');
 
         }
-    });
+    })
 
 
-
+    
     // 상세정보 준비 안내 창
     $('.muji .inner > a').click(function(e){
 
